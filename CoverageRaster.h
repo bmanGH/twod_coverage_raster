7478a8d1dfb0687d80939a34ba806858ea67c07c
bman <zx123xz321hm3@hotmail.com>
2014-03-06 20:10:59 +0800

    Update CoverageRaster.h

--- CoverageRaster.h
+++ CoverageRaster.h
@@ -1,6 +1,8 @@
 #ifndef _RasterizeCoverage_H_
 #define _RasterizeCoverage_H_
 
+// fork from https://github.com/aras-p/twod_coverage_raster
+
 // Rasterize 2D triangles into 1 byte-per-pixel image.
 // @param indices Index buffer, 3 ints per triangle.
 // @param vertices Vertices, 2 floats per vertex; 0..1 range over the image.
